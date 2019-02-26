06f73029fd24e75f229ba64c4b0582934e3f8017
celbax95 <loicmace95@gmail.com>
2019-02-26 08:32:26 +0100

    Ajout User

---
+++ src/user/User.java
@@ -0,0 +1,18 @@
+package user;
+
+public class User {
+
+	private String login;
+	private String pass;
+
+	public User() {
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+}
