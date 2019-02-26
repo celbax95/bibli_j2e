ae4ae2db2df69a0dc507f391896d042874c596df
celbax95 <loicmace95@gmail.com>
2019-02-26 08:34:12 +0100

    Ajout constructeur User

--- src/user/User.java
+++ src/user/User.java
@@ -5,7 +5,9 @@ public class User {
 	private String login;
 	private String pass;
 
-	public User() {
+	public User(String login, String pass) {
+		this.login = login;
+		this.pass = pass;
 	}
 
 	public String getLogin() {
