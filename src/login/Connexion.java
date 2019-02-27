c9bdae1ced7270785556ebff471a78bdfa460e5c
celbax95 <loicmace95@gmail.com>
2019-02-27 15:25:57 +0100

    remove outils de connexion de Connexion

--- src/login/Connexion.java
+++ src/login/Connexion.java
@@ -1,9 +1,6 @@
 package login;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -11,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import loader.Loader;
 import mediatheque.Mediatheque;
 import user.User;
 
@@ -19,25 +15,8 @@ public class Connexion extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	private static Connection connectMySQL(String url, String log, String mdp) {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-
-		try {
-			return DriverManager.getConnection(url, log, mdp);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		Connection c = connectMySQL(Loader.URL, Loader.LOG, Loader.MDP);
 
 		Mediatheque m = Mediatheque.getInstance();
 
