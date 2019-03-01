62263686e1de3ee83d4fa119d80fe2fd7db03de8
celbax95 <loicmace95@gmail.com>
2019-03-01 14:37:14 +0100

    add rendre document

---
+++ WEB-INF/vueEmprunt.jsp
@@ -0,0 +1,93 @@
+<%@ page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ page import ="java.util.*" %>
+<%@ page import ="user.User" %>
+<%@ page import ="mediatheque.Document" %>
+
+<%
+	List<Document> docs = (List<Document>)request.getAttribute("docs");
+	String nomUser = ((User)request.getSession().getAttribute("user")).getLogin();
+	nomUser = Character.toUpperCase(nomUser.charAt(0)) + nomUser.substring(1, nomUser.length());
+	
+	String rech = request.getParameter("rech")!=null?(String)request.getParameter("rech").toLowerCase():null;
+	String[] rechT = rech!=null?rech.split("\\+"):null;
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Vue Mediatheque</title>
+	<link rel="stylesheet" type="text/css" href="./css/body.css">
+	<link rel="stylesheet" type="text/css" href="./css/user.css">
+	<link rel="stylesheet" type="text/css" href="./css/titre.css">
+	<link rel="stylesheet" type="text/css" href="./css/vueMedia.css">
+</head>
+
+<body>
+	<div id="user">
+		<a href="/bibli_j2e/log">
+			<div></div>
+			<div><p><%=nomUser%></p></div>
+		</a>
+	</div>
+
+	<div id="titre"><a href="/bibli_j2e/menu">
+	<h1>Média2e</h1>
+	</a></div>
+
+	<div id="content">
+		<div id="list">
+			<form action="" method="get" id="rechForm">
+				<label for="rech">Rechercher : </label>
+				<input id="rech" type="text" name="rech" value="<%=rech!=null?rech:""%>">
+				<button type="submit"></button>
+			</form>
+
+			<div id="docList">
+				<%for (Document d : docs) {
+				Object[] o = d.affiche();
+				
+				if (rechT != null) {
+					boolean ct = false;
+					for (int i = 0, c = o.length; i < c; i++) {
+						for (int j = 0, c2 = rechT.length; j < c2; j++) {
+							if (o[i].toString().toLowerCase().contains(rechT[j])) {
+								ct = true;
+								break;
+							}
+						}
+						if (ct)
+							break;
+					}
+					if (!ct)
+						continue;
+				}
+
+				%>
+
+				<div class="doc">
+					<div class="stdDocInfo">
+						<div class="inline cCourt"><p><%=o[2].toString()%></p></div>
+						<div class="inline cLong"><p><%=o[3].toString()%></p></div>
+						<div class="inline cLong"><p><%=o[4].toString()%></p></div>
+						<div class="inline cCourt"><p><%=o[5].toString()%></p></div>
+					</div>
+					<div class="allDocInfo">
+						<div class="inlineInfo">
+							<pre><%=o[2].toString()%> :</pre>
+							<%for (int i = 3, c2 = o.length; i < c2; i++) {%>
+								<pre> <%=o[i].toString() + (((i+1)<c2)?",":"")%></pre>
+							<%}%>
+							<div class="action">
+								<a href="?idDoc=<%=o[1].toString()%>&rech=<%=rech!=null?rech:""%>">
+									<div class="btn active"><p>Rendre</p></div>
+								</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<%}%>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
--- src/vuemedia/VueEmprunt.java
+++ src/vuemedia/VueEmprunt.java
@@ -4,8 +4,11 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -14,6 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import loader.Loader;
+import mediatheque.Document;
 import mediatheque.EmpruntException;
 import mediatheque.Mediatheque;
 import user.User;
@@ -34,37 +38,82 @@ public class VueEmprunt extends HttpServlet {
 
 		Integer idDoc = (req.getParameter("idDoc") != null ? Integer.valueOf((req.getParameter("idDoc"))) : null);
 
+		String rech = req.getParameter("rech");
+
 		if (idDoc != null) {
-			emprunter(u, idDoc);
-			resp.sendRedirect("./vueMedia");
+			retour(idDoc);
+			resp.sendRedirect("./vueEmprunt" + (rech != null ? "?rech=" + rech : ""));
 			return;
 		}
 
-		req.setAttribute("bibli", u.isBibliothecaire());
-		req.setAttribute("docs", Mediatheque.getInstance().tousLesDocuments());
+		req.setAttribute("rech", rech);
 
-		this.getServletContext().getRequestDispatcher("/WEB-INF/vueMedia.jsp").forward(req, resp);
+		req.setAttribute("bibli", u.isBibliothecaire());
+
+		List<Document> ld = Mediatheque.getInstance().tousLesDocuments();
+
+		List<Integer> le = getEmprunts(u.getId());
+
+		if (le != null) {
+			for (int i = 0, c = ld.size(); i < c; i++) {
+				Object[] o = ld.get(i).affiche();
+				boolean ct = false;
+				for (Integer in : le) {
+					if ((Boolean) o[0] == true && (Integer) o[1] == in) {
+						ct = true;
+						break;
+					}
+				}
+				if (!ct) {
+					ld.remove(i--);
+					c--;
+				}
+			}
+		}
+
+		req.setAttribute("docs", ld);
+
+		this.getServletContext().getRequestDispatcher("/WEB-INF/vueEmprunt.jsp").forward(req, resp);
 	}
 
-	private void emprunter(User u, Integer idDoc) {
-		String req = "INSERT INTO emprunt VALUES(?,?)";
+	private List<Integer> getEmprunts(int idUser) {
+		String req = "Select idDoc FROM Emprunt WHERE idUtilisateur = ?";
+		Connection c = connectMySQL(Loader.URL, Loader.LOG, Loader.MDP);
+
+		ResultSet res;
+
+		List<Integer> a = new ArrayList<>();
+
+		try {
+			PreparedStatement s = c.prepareStatement(req);
+			s.setInt(1, idUser);
+			res = s.executeQuery();
+
+			while (res.next()) {
+				a.add(res.getInt(1));
+			}
+			return a;
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private void retour(Integer idDoc) {
+		String req = "DELETE FROM Emprunt WHERE idDoc = ?";
 		Connection c = connectMySQL(Loader.URL, Loader.LOG, Loader.MDP);
 
 		try {
 			PreparedStatement s = c.prepareStatement(req);
 			s.setInt(1, idDoc);
-			s.setInt(2, u.getId());
 			s.executeUpdate();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 
 		Mediatheque m = Mediatheque.getInstance();
-		try {
-			m.emprunt(m.getDocument(idDoc), u);
-		} catch (EmpruntException e) {
-			System.out.println("Impossible d'emprunter ce document.");
-		}
+		m.retour(m.getDocument(idDoc));
 	}
 
 	public static Connection connectMySQL(String url, String log, String mdp) {
