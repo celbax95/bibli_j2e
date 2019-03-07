4a0a350d1d7334987280d61eb5edb3fa1d02bf7d
celbax95 <loicmace95@gmail.com>
2019-03-07 13:50:33 +0100

    modify liste deroulante

--- javascript/vueAddDoc.js
+++ javascript/vueAddDoc.js
@@ -1,5 +1,12 @@
 window.onload = function() {
 	var l = document.querySelector("#content div.list div.noScroll");
-	if (l.offsetHeight > 200)
-		l.style.height = "200px";
+	var h = l.offsetHeight-1;
+	if (h > 300)
+		l.style.height = "301px";
+	else if (h > 150)
+		l.style.height = "151px";
+	else if (h > 50)
+		l.style.height = "101px";
+	else
+		l.style.height = "51px";
 };
